d96591c67c6707f40b4c3468aba92b6b270be0a5
Radim Krčmář <rkrcmar@ventanamicro.com>
2026-07-20 19:36:44 -0600

    RISC-V: make use of variadic sbi_ecall
    The new sbi_ecall doesn't have to list all 8 arguments anymore, so only
    pass the actual numbers of arguments for each SBI function.
    
    The remaining trailing 0 are intentional as they represent an argument.
    
    SBI 0.1 shouldn't be using the sbi_ecall, because it's only for 0.2+,
    but allow it by passing 0 is the reserved register.
    
    Signed-off-by: Radim Krčmář <rkrcmar@ventanamicro.com>
    Link: https://patch.msgid.link/20250619190315.2603194-3-rkrcmar@ventanamicro.com

--- arch/riscv/kernel/suspend.c
+++ arch/riscv/kernel/suspend.c
@@ -116,7 +116,7 @@ static int sbi_system_suspend(unsigned long sleep_type,
 	struct sbiret ret;
 
 	ret = sbi_ecall(SBI_EXT_SUSP, SBI_EXT_SUSP_SYSTEM_SUSPEND,
-			sleep_type, resume_addr, opaque, 0, 0, 0);
+			sleep_type, resume_addr, opaque);
 	if (ret.error)
 		return sbi_err_map_linux_errno(ret.error);
 
@@ -154,7 +154,7 @@ static int sbi_suspend_finisher(unsigned long suspend_type,
 	struct sbiret ret;
 
 	ret = sbi_ecall(SBI_EXT_HSM, SBI_EXT_HSM_HART_SUSPEND,
-			suspend_type, resume_addr, opaque, 0, 0, 0);
+			suspend_type, resume_addr, opaque);
 
 	return (ret.error) ? sbi_err_map_linux_errno(ret.error) : 0;
 }
